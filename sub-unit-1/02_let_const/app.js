09c474c8d4e9db75851e7b7fb7a8124ae7de0bd8
Jacob Gordon <jgordon024@west-mec.org>
2019-08-19 18:18:46 -0700

    First Commit, app.js
    Created two variables and a constant function that demonstrates block scope

---
+++ sub-unit-1/02_let_const/app.js
@@ -0,0 +1,18 @@
+"use script";
+var a='test1'
+var b='test2'
+
+console.log(`Logged: ${a}`);
+
+const testVar = (params) => {
+    let a = 30;
+    console.log(`Function logged: ${a}`);
+    if(true){
+        var a = 50;
+        console.log(`Block logged: `${a});
+    }
+    console.log("Here:"+a);
+}
+
+testVar();
+console.log(`END logged: ${a}`);
