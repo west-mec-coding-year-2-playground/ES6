b9b7db69e176b7004d03321a18762da00629feb9
Hans <brett.mcmurdy@west-mec.org>
2019-08-21 11:02:20 -0700

    update

--- sub-unit-1/01_babel/src/main.js
+++ sub-unit-1/01_babel/src/main.js
@@ -1,3 +1,7 @@
+const testVar = (params) => {
+    
+}
+
 
 
 class User{
@@ -5,7 +9,11 @@ class User{
     console.log('User Registered...');
   }
 }
+
+
+
 const colors = [];
+
 
 colors.push('red');
 colors.push('blue');
@@ -15,3 +23,10 @@ colors.push('blue');
 console.log(colors);
 
 
+
+const name = () => {
+  console.log(`Hi Juan & Lenny!`);
+  
+}
+
+name();
