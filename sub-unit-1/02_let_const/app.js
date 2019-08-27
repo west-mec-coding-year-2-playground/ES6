b2d4ffc816f7b68163e7f796a33ef4c0178ad124
Todd Adcox <TAdcox009@west-mec.org>
2019-08-26 18:14:55 -0700

    update

--- sub-unit-1/02_let_const/app.js
+++ sub-unit-1/02_let_const/app.js
@@ -1,16 +1,33 @@
-"use script";
-var a = 'Test1';
-var b = 'Test2';
-console.log(`Logged: ${a}`);
-const testVar = (params) => {
-   let a = 30;
-   console.log(`Function Logged: ${a}`);
-   if(true) {
-       var a = 50;
-       console.log(`Block Logged: ${a}`);
-   }
-   console.log("here: "+a);
-}
-testVar();
-console.log(`END Logged: ${a}`);
-Collapse
+let LOG = console.log;
+
+
+
+const pizza = [1,2,3];
+
+console.log(pizza);
+
+
+const pizza = [
+    {
+        name: "pep",
+        number: 3,
+        price: 5
+    },
+    {
+        name: "mushroom",
+        number: 3,
+        price: 5
+    },
+    {
+        name: "mango",
+        number: 3,
+        price: 5
+    }
+];
+
+
+
+
+const [pep, mushroom] = pizza;
+
+LOG(pep.name);
