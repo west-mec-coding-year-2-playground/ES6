aee1ceb597d5c6d031243203ae834c3c15fb920e
astilphen000 <astilphen000@west-mec.org>
2019-08-26 17:50:35 -0700

    let and const exercise

--- sub-unit-1/02_let_const/app.js
+++ sub-unit-1/02_let_const/app.js
@@ -1,15 +1,42 @@
-"use script";
-var a = 'Test1';
-var b = 'Test2';
-console.log(`Logged: ${a}`);
-const testVar = (params) => {
-    let a = 30;
-    console.log(`Function Logged: ${a}`);
-    if(true) {
-        var a = 50;
-        console.log(`Block Logged: ${a}`);
-    }
-    console.log("here: "+a);
-}
-testVar();
-console.log(`END Logged: ${a}`);
+let LOG = console.log ;
+
+
+const pizza = [1,2,3];
+
+console.log(pizza);
+
+const pizza = [
+    {
+        name: "pep", 
+        number: 3,
+        price: 5
+
+    },
+    {
+        name: "mushroom",
+        number: 3,
+        price: 5
+    },
+    {
+        name: "mango",
+        number: 3,
+        price: 5
+    },
+];
+
+
+// "use script";
+// var a = 'Test1';
+// var b = 'Test2';
+// console.log(`Logged: ${a}`);
+// const testVar = (params) => {
+//     let a = 30;
+//     console.log(`Function Logged: ${a}`);
+//     if(true) {
+//         var a = 50;
+//         console.log(`Block Logged: ${a}`);
+//     }
+//     console.log("here: "+a);
+// }
+// testVar();
+// console.log(`END Logged: ${a}`);
